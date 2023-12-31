5d67eb35bfc6c7bb467c70bebdd44ece7622868d
Prince Kumar <princepk7878kumar@gmail.com>
2023-12-31 18:00:28 +0530

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -44,7 +44,7 @@ export default {
       "nord",
       "sunset",
     ], 
-    darkTheme: "sunset", // name of one of the included themes for dark mode
+    darkTheme: "dark", // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes
